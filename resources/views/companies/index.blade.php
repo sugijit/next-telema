485fc6fdbedaf82d2358c720acada3859b597c5a
sugijit <b.suldsaikhan@gmail.com>
2024-09-11 20:57:22 +0900

    users.index, companies.indexにタイトル

--- resources/views/companies/index.blade.php
+++ resources/views/companies/index.blade.php
@@ -3,7 +3,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <div class="mb-6 text-right">
+                    <div class="mb-6 flex justify-between">
+                        <div><i class="text-3xl fa-solid fa-building text-gray-400 ml-2"></i><span class="text-xl ml-4 text-gray-400">企業一覧</span></div>
                         <a href="{{ route('companies.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                             企業追加
                         </a>
