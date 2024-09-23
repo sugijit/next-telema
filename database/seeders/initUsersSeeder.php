b888ace2ead817b0d4f47e7d68ed06dc906c37ee
sugijit <b.suldsaikhan@gmail.com>
2024-09-23 12:37:46 +0900

    nl seeder

---
+++ database/seeders/initUsersSeeder.php
@@ -0,0 +1,178 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str; 
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
+
+class initUsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => "システム管理者",
+                'user_cd' => "admin",
+                'company_id' => "1",
+                'role' => "nl_admin",
+                'email' => "b.suldsaikhan@gmail.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('admin_dev'),
+                'pass_decrypt' => "admin_dev",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "森屋敷暁",
+                'user_cd' => "nextlink_a",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "a.moriyashiki@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Akira0728'),
+                'pass_decrypt' => "Akira0728",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "半田真彩",
+                'user_cd' => "nextlink_hm",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "m.handa@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Maya0000'),
+                'pass_decrypt' => "Maya0000",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "井上海斗",
+                'user_cd' => "nextlink_ik",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "k.inoue@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Taka1230'),
+                'pass_decrypt' => "Taka1230",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "板井瑞季",
+                'user_cd' => "nextlink_im",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "m.itai@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Taka1230'),
+                'pass_decrypt' => "Taka1230",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "岩崎 裕也",
+                'user_cd' => "nextlink_iy",
+                'company_id' => "1",
+                'role' => "admin",
+                'email' => "y.iwasaki@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Yuya0809'),
+                'pass_decrypt' => "Yuya0809",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "垣内崇広",
+                'user_cd' => "nextlink_k",
+                'company_id' => "1",
+                'role' => "admin",
+                'email' => "t.kakiuchi@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Taka1230'),
+                'pass_decrypt' => "Taka1230",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "増井美樹",
+                'user_cd' => "nextlink_m",
+                'company_id' => "1",
+                'role' => "admin",
+                'email' => "m.masui@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Miki0103'),
+                'pass_decrypt' => "Miki0103",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "森川詠",
+                'user_cd' => "nextlink_mu",
+                'company_id' => "1",
+                'role' => "admin",
+                'email' => "u.morikawa@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Taka1230'),
+                'pass_decrypt' => "Taka1230",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "白井良子",
+                'user_cd' => "nextlink_sr",
+                'company_id' => "1",
+                'role' => "admin",
+                'email' => "r.shirai@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Ryoko0710'),
+                'pass_decrypt' => "Ryoko0710",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "臼井令",
+                'user_cd' => "nextlink_u",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "r.usui@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Taka1230'),
+                'pass_decrypt' => "Taka1230",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'name' => "渡壁麗香",
+                'user_cd' => "nextlink_w",
+                'company_id' => "1",
+                'role' => "user",
+                'email' => "r.watakabe@nl-nextlink.com",
+                'email_verified_at' => now(),
+                'password' => Hash::make('Reika0424'),
+                'pass_decrypt' => "Reika0424",
+                'remember_token' => Str::random(10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
